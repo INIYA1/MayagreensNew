6acda42b1269828623f68030a495f6a8ca4e1d5c
INIYA <iniya.arumugam@dreamguystech.com>
2023-05-18 18:51:36 +0530

    motoronesensor

--- src/components/IrrigationPageList/MotoroneSensor.js
+++ src/components/IrrigationPageList/MotoroneSensor.js
@@ -1,27 +1,33 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+const sensorWaterlevel=()=>{
+  return(
+    <View style={styles.sensorContainer}>
+    <View style={{ flexDirection: 'row' }}>
+      <Text style={{ color: 'black', left: 5, top: 5, fontSize: 15 }}>Water Level</Text>
+      <Image source={require('../../images/tower-red-icon.png')} style={{ height: 30, width: 30, top: 5, left: 80 }} />
+    </View>
+    <View style={{flexDirection:'row',justifyContent:'space-around'}}>
+    <Image source={require('../../images/water_level_50.png')} style={{ height: 60, width: 60, top: 5, }} />
+    <Image source={require('../../images/graph.png')} style={{ height: 35, width: 35, top: 30, }} />
+    <Image source={require('../../images/battery_0.png')} style={{ height: 35, width: 35, top: 30,  }} />
+    </View>
+    <View style={{flexDirection:'row',top:10,justifyContent:'space-around',left:8}}>
+      <Text style={{color:'black', fontWeight:'500'}}>50%</Text>
+      <Text style={{color:'black', fontWeight:'500'}}>View</Text>
+      <Text style={{color:'black', fontWeight:'500'}}>0%</Text>
+    </View>
+  </View>
+  )
+}
+
 export default function MotoroneSensor() {
 
         return (
           <View>
           <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-            <View style={styles.sensorContainer}>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ color: 'black', left: 5, top: 5, fontSize: 15 }}>Water Level</Text>
-                <Image source={require('../../images/tower-red-icon.png')} style={{ height: 30, width: 30, top: 5, left: 80 }} />
-              </View>
-              <View style={{flexDirection:'row',justifyContent:'space-around'}}>
-              <Image source={require('../../images/water_level_50.png')} style={{ height: 60, width: 60, top: 5, }} />
-              <Image source={require('../../images/graph.png')} style={{ height: 35, width: 35, top: 30, }} />
-              <Image source={require('../../images/battery_0.png')} style={{ height: 35, width: 35, top: 30,  }} />
-              </View>
-              <View style={{flexDirection:'row',top:10,justifyContent:'space-around',left:8}}>
-                <Text style={{color:'black', fontWeight:'500'}}>50%</Text>
-                <Text style={{color:'black', fontWeight:'500'}}>View</Text>
-                <Text style={{color:'black', fontWeight:'500'}}>0%</Text>
-              </View>
-            </View>
+{sensorWaterlevel()}
             <View style={styles.sensorContainer}>
             <View style={{ flexDirection: 'row' }}>
                 <Text style={{ color: 'black', left: 5, top: 5, fontSize: 15 }}>Pressure</Text>
